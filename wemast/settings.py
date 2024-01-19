2a686ae344771297019766c8c08441cbdd835046
Seth Nyawacha <50513411+sethgis@users.noreply.github.com>
2024-01-19 12:14:02 +0300

    Update settings.py

--- wemast/settings.py
+++ wemast/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = "django-insecure-pq))&fdmah4n2uzy+^9llde-=dpf&hx2(hz3!^to_3ionm&$c-"
+# SECRET_KEY = "-"
 # SECRET_KEY = os.env['SECRET_KEY']
-SECRET_KEY = "django-insecure-pq))&fdmah4n2uzy+^9llde-=dpf&hx2(hz3!^to_3ionm&$c-"
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
